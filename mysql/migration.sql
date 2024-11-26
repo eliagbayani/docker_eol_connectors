5607b4b685c06dc2650cd2556bb411abba4f609d
eliagbayani <eagbayani@eol.org>
2024-11-27 02:31:41 +0800

    Create migration.sql

---
+++ mysql/migration.sql
@@ -0,0 +1,38 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : docker_mysql_port_4001
+ Source Server Type    : MySQL
+ Source Server Version : 80403
+ Source Host           : localhost
+ Source Database       : Employees
+
+ Target Server Type    : MySQL
+ Target Server Version : 80403
+ File Encoding         : utf-8
+
+ Date: 11/27/2024 00:18:54 AM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `employees_tbl`
+-- ----------------------------
+DROP TABLE IF EXISTS `employees_tbl`;
+CREATE TABLE `employees_tbl` (
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `emp_id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`emp_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+--  Records of `employees_tbl`
+-- ----------------------------
+BEGIN;
+INSERT INTO `employees_tbl` VALUES ('Jen', 'Hammock', '2'), ('Katja', 'Schulz', '3'), ('Jeremy', 'Rice', '4'), ('Eli', 'Agbayani', '9');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
