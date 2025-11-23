d6b12bcc15f07697df137928a1bb8fc6d30b0121
eliagbayani <eagbayani@eol.org>
2025-11-23 23:39:07 +0800

    Update executors.groovy

--- jenkins/executors.groovy
+++ jenkins/executors.groovy
@@ -1,2 +1,2 @@
 import jenkins.model.*
-Jenkins.instance.setNumExecutors(7) // Recommended to not run builds on the built-in node
+Jenkins.instance.setNumExecutors(15) // Recommended to not run builds on the built-in node
