67176ceb660dd0762308ea0c3bb9d222e2e34a27
eliagbayani <eagbayani@eol.org>
2026-05-19 01:38:32 +0800

    Update docker-entrypoint_development.sh

--- apache-php/docker-entrypoint_development.sh
+++ apache-php/docker-entrypoint_development.sh
@@ -14,11 +14,11 @@ cp /tmp/info.php ${TARGET_PATH}
 # ========== Here add all symlinks needed
 # cd ${TARGET_PATH} #not needed
 echo "symlimk start... ${MY_ENV}"
+[ -d ${TARGET_PATH}/opendata ]          && echo "Symlink already exists (opendata)"         || ln -s /opt/homebrew/var/www/eol_php_code/applications/opendata/ opendata 
 [ -d ${TARGET_PATH}/eoearth_images ]    && echo "Symlink already exists (eoearth_images)"   || ln -s /Volumes/AKiTiO4/web/eoearth_images/ eoearth_images
 [ -d ${TARGET_PATH}/eoearth ]           && echo "Symlink already exists (eoearth)"          || ln -s /Volumes/AKiTiO4/webroot/eoearth/ eoearth
 [ -d ${TARGET_PATH}/maps_test ]         && echo "Symlink already exists (maps_test)"        || ln -s /Volumes/AKiTiO4/webroot/maps_test/ maps_test
 [ -d ${TARGET_PATH}/eol_maps ]          && echo "Symlink already exists (eol_maps)"         || ln -s /Volumes/AKiTiO4/webroot/eol_maps/ eol_maps
-[ -d ${TARGET_PATH}/opendata ]          && echo "Symlink already exists (opendata)"         || ln -s /opt/homebrew/var/www/eol_php_code/applications/opendata/ opendata 
 [ -d ${TARGET_PATH}/opendata_uploads ]  && echo "Symlink already exists (opendata_uploads)" || ln -s /Volumes/AKiTiO4/other_files/opendata_uploads/ opendata_uploads
 [ -d ${TARGET_PATH}/other_files ]       && echo "Symlink already exists (other_files)"      || ln -s /Volumes/AKiTiO4/other_files/ other_files
 [ -d ${TARGET_PATH}/d_w_h ]             && echo "Symlink already exists (d_w_h)"            || ln -s /Volumes/AKiTiO4/d_w_h/dynamic_working_hierarchy-master/ d_w_h 
@@ -28,9 +28,9 @@ echo "symlimk start... ${MY_ENV}"
 [ -d ${TARGET_PATH}/ckan_api_results ]  && echo "Symlink already exists (ckan_api_results)" || ln -s /Volumes/AKiTiO4/CKAN_info/api_results/ ckan_api_results
 [ -d ${TARGET_PATH}/wikimedia_cache ]   && echo "Symlink already exists (wikimedia_cache)"  || ln -s /Volumes/AKiTiO4/wikimedia_cache/ wikimedia_cache
 [ -d ${TARGET_PATH}/Leaflet_Cluster_map ]       && echo "Symlink already exists (Leaflet_Cluster_map)"    || ln -s /Volumes/AKiTiO4/webroot/Leaflet_Cluster_map/ Leaflet_Cluster_map
-[ -d ${TARGET_PATH}/Pensoft_annotator ]         && echo "Symlink already exists (Pensoft_annotator)"      || ln -s /Volumes/Crucial_4TB/Pensoft_annotator/ Pensoft_annotator
 [ -d ${TARGET_PATH}/other_files2 ]              && echo "Symlink already exists (other_files2)"           || ln -s /Volumes/Crucial_2TB/other_files2/ other_files2
 [ -d ${TARGET_PATH}/cache_LiteratureEditor ]    && echo "Symlink already exists (cache_LiteratureEditor)" || ln -s /Volumes/Crucial_2TB/cache_LiteratureEditor/ cache_LiteratureEditor
+[ -d ${TARGET_PATH}/Pensoft_annotator ]         && echo "Symlink already exists (Pensoft_annotator)"      || ln -s /Volumes/Crucial_4TB/Pensoft_annotator/ Pensoft_annotator
 [ -d ${TARGET_PATH}/gnfinder ]                  && echo "Symlink already exists (gnfinder)"               || ln -s /Volumes/Crucial_4TB/gnfinder/ gnfinder
 
 # These 2 are real folders for PHP 8, not symlinks.
