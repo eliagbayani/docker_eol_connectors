fd89e5b2dd9400a5e650e72265f81d9dc2408d89
eliagbayani <eagbayani@eol.org>
2026-06-21 01:11:22 +0800

    Update test_MySQL_db.sql

--- mysql/test_MySQL_db.sql
+++ mysql/test_MySQL_db.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `employees_tbl` (
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   PRIMARY KEY (`emp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
